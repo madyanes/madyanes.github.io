aa59b2444ddfdca796ac268065f5e2e3fa5c1564
madyanes <madyanes.dev@gmail.com>
2024-09-25 00:37:05 +0700

    Setting up ESLint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import eslintPluginAstro from 'eslint-plugin-astro'
+
+// Docs can be seen here: https://ota-meshi.github.io/eslint-plugin-astro/user-guide/
+
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+
+      'astro/no-unused-css-selector': 'warn',
+    },
+  },
+]
