f551d53de56a7f51e73ba0e98db4fda68c1fed6a
madyanes <madyanes.dev@gmail.com>
2024-09-30 08:58:59 +0700

    Refactor
    1. Using MDX instead of MD
    2. Positioning metadata below the article

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,12 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://madyanes.github.io",
-  integrations: [tailwind(), mdx()],
+  integrations: [
+    tailwind(),
+    mdx({
+      gfm: true,
+    }),
+  ],
   markdown: {
     shikiConfig: {
       theme: "github-light",
