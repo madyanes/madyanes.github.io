6c8efcb96b6ab7a533970b0b2fb135f3c2980012
madyanes <madyanes.dev@gmail.com>
2024-10-01 09:18:15 +0700

    React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,14 @@ import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://madyanes.github.io",
-  integrations: [
-    tailwind(),
-    mdx({
-      gfm: true,
-    }),
-  ],
+  integrations: [tailwind(), mdx({
+    gfm: true,
+  }), react()],
   markdown: {
     shikiConfig: {
       theme: "github-light",
